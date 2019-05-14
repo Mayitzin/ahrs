7cd552a4624eb9e6d0841811e47fdd9c0abc4926
Admin <mayitzin@gmail.com>
2019-05-14 15:33:37 +0200

    Add, test and plot Madgwick's algorithm.
    - Use Madgwick's routine for q2euler.
    - Use a ndarray product to compute the conjugate of a quaternion in q_conj.
    - Add functions updateIMU and updateMARG in module Madgwick.
    - Test and plot Madgwick's algorithm in test script.
    - Add ExampleData.mat from Madgwick's work.

--- ahrs/filters/madgwick.py
+++ ahrs/filters/madgwick.py
@@ -5,20 +5,80 @@ Filtering routines
 """
 
 import numpy as np
+from ahrs.common.orientation import *
 
-def madgwick(x):
+def updateIMU(gyr, acc, q, beta=0.1, freq=1.0/256.0):
+    """Non-optimized Madgwick's AHRS algorithm with a IMU architecture.
+
+    Adapted to Python from original implementation by Sebastian Madgwick.
+
+    See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
+    @author: Sebastian Madgwick (2011)
+    See: http://www.olliw.eu/2013/imu-data-fusing/
+         https://motsai.com/omid-vs-madgwick-low-power-orientation-filters/
     """
-    Return the Madgwick routine.
+    acc /= np.linalg.norm(acc)
+    qw, qx, qy, qz = q[0], q[1], q[2], q[3]
+    # Gradient decent algorithm corrective step
+    F = np.asarray([[2.0*(qx*qz - qw*qy)   - acc[0]],
+                    [2.0*(qw*qx + qy*qz)   - acc[1]],
+                    [2.0*(0.5-qx**2-qy**2) - acc[2]]])
+    J = np.asarray([[-2.0*qy, 2.0*qz, -2.0*qw, 2.0*qx],
+                    [ 2.0*qx, 2.0*qw,  2.0*qz, 2.0*qy],
+                    [ 0.0,   -4.0*qx, -4.0*qy, 0.0   ]])
+    step = np.dot(J.transpose(), F)
+    step /= np.linalg.norm(step)
+    # Compute rate of change of quaternion
+    qDot = 0.5 * q_prod(q, [0, gyr[0], gyr[1], gyr[2]]) - beta * step.transpose()
+    # Integrate to yield Quaternion
+    q = q + qDot/freq
+    q /= np.linalg.norm(q)
+    return q[0]
 
-    Parameters
-    ----------
-    x : float
-        Data
 
-    Returns
-    -------
-    y : float
-        Output
+def updateMARG(gyr, a, m, q, beta=0.1, samplePeriod=1.0/256.0):
+    """Non-optimized Madgwick's AHRS algorithm with a MARG architecture.
 
+    Adapted to Python from original implementation by Sebastian Madgwick.
+
+    See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
+    @author: Sebastian Madgwick (2011)
+    See: http://www.olliw.eu/2013/imu-data-fusing/
+         https://motsai.com/omid-vs-madgwick-low-power-orientation-filters/
     """
-    return 2.0*x
+    qw, qx, qy, qz = q[0], q[1], q[2], q[3]
+    # Normalise accelerometer measurement
+    a_norm = np.linalg.norm(a)
+    if a_norm == 0:     # handle NaN
+        return q
+    a /= a_norm
+    # Normalise magnetometer measurement
+    m_norm = np.linalg.norm(m)
+    if m_norm == 0:     # handle NaN
+        return q
+    m /= m_norm
+    # Reference direction of Earth's magnetic field
+    h = q_prod(q, q_prod([0, m[0], m[1], m[2]], q_conj(q)))
+    b = [0.0, np.linalg.norm([h[1], h[2]]), 0.0, h[3]]
+    # Gradient decent algorithm corrective step    
+    F = np.array([2.0*(qx * qz - qw * qy)   - a[0],
+                  2.0*(qw * qx + qy * qz)   - a[1],
+                  2.0*(0.5 - qx**2 - qy**2) - a[2],
+                  2.0*b[1]*(0.5 - qy**2 - qz**2) + 2.0*b[3]*(qx*qz - qw*qy)       - m[0],
+                  2.0*b[1]*(qx*qy - qw*qz)       + 2.0*b[3]*(qw*qx + qy*qz)       - m[1],
+                  2.0*b[1]*(qw*qy + qx*qz)       + 2.0*b[3]*(0.5 - qx**2 - qy**2) - m[2]])
+    J = np.array([[-2.0*qy,                 2.0*qz,                  -2.0*qw,                    2.0*qx],
+                  [ 2.0*qx,                 2.0*qw,                   2.0*qz,                    2.0*qy],
+                  [ 0.0,                   -4.0*qx,                  -4.0*qy,                    0.0],
+                  [-2.0*b[3]*qy,            2.0*b[3]*qz,             -4.0*b[1]*qy-2.0*b[3]*qw,  -4.0*b[1]*qz+2.0*b[3]*qx],
+                  [-2.0*b[1]*qz+2*b[3]*qx,  2.0*b[1]*qy+2.0*b[3]*qw,  2.0*b[1]*qx+2.0*b[3]*qz,  -2.0*b[1]*qw+2.0*b[3]*qy],
+                  [ 2.0*b[1]*qy,            2.0*b[1]*qz-4.0*b[3]*qx,  2.0*b[1]*qw-4.0*b[3]*qy,   2.0*b[1]*qx]])
+    step = J.T@F
+    step /= np.linalg.norm(step)    # normalise step magnitude
+    # Compute rate of change of quaternion
+    qDot = 0.5 * q_prod(q, [0, gyr[0], gyr[1], gyr[2]]) - beta * step.T
+    # Integrate to yield quaternion
+    q += qDot*samplePeriod
+    q /= np.linalg.norm(q) # normalise quaternion
+    return q
+
--- test.py
+++ test.py
@@ -5,6 +5,37 @@ Test File
 
 """
 
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
 import ahrs
 
-print("Test File")
+RAD2DEG = ahrs.common.mathfuncs.RAD2DEG
+DEG2RAD = ahrs.common.mathfuncs.DEG2RAD
+
+data = sio.loadmat('ExampleData.mat')
+time = data['time']
+accs = data['Accelerometer']
+gyrs = data['Gyroscope']
+mags = data['Magnetometer']
+sensors = ['Gyroscope', 'Accelerometer', 'Magnetometer']
+
+num_samples = len(time)
+Q = np.tile([1., 0., 0., 0.], (num_samples, 1))
+euler_angles = np.zeros((num_samples, 3))
+q = Q[0].copy()
+for t, ts in enumerate(time):
+    q = ahrs.filters.madgwick.updateMARG(DEG2RAD*gyrs[t], accs[t], mags[t], q)
+    Q[t] = q.copy()
+    euler_angles[t] = ahrs.common.orientation.q2euler(ahrs.common.orientation.q_conj(q))*RAD2DEG
+
+# Figure 1
+fig1 = plt.figure()
+for n, p in enumerate(sensors):
+    fig1.add_subplot(len(sensors), 1, n+1, title=p, xlabel='Time')
+    for sensor, sensor_data in data.items():
+        plt.plot(time, data[p][:, 0], 'r-', time, data[p][:, 1], 'g-', time, data[p][:, 2], 'b-', linewidth=0.2)
+# Figure 2
+fig2 = plt.figure()
+plt.plot(time, euler_angles[:,0], 'r-', time, euler_angles[:,1], 'g-', time, euler_angles[:,2], 'b-', linewidth=0.2)
+plt.show()
