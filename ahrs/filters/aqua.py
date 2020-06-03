Fix Algebraic Quaternion Algorithm

ahrs/filters/aqua.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 """
 Algebraic Quaternion Algorithm
+==============================
 
 References
 ----------
-.. [Valenti1] Roberto Valenti et al. (2015) A Linear Kalman Filter for MARG
-    Orientation Estimation Using the Algebraic Quaternion Algorithm.
-    https://ieeexplore.ieee.org/document/7345567
-.. [Valenti2] Valenti, R.G.; Dryanovski, I.; Xiao, J. Keeping a Good Attitude:
-    A Quaternion-Based Orientation Filter for IMUs and MARGs. Sensors 2015, 15,
-    19302-19330.
-    https://res.mdpi.com/sensors/sensors-15-19302/article_deploy/sensors-15-19302.pdf
+.. [1] Valenti, R.G.; Dryanovski, I.; Xiao, J. Keeping a Good Attitude: A
+   Quaternion-Based Orientation Filter for IMUs and MARGs. Sensors 2015, 15,
+   19302-19330.
+   (https://res.mdpi.com/sensors/sensors-15-19302/article_deploy/sensors-15-19302.pdf)
+.. [2] R. G. Valenti, I. Dryanovski and J. Xiao, "A Linear Kalman Filter for
+   MARG Orientation Estimation Using the Algebraic Quaternion Algorithm," in
+   IEEE Transactions on Instrumentation and Measurement, vol. 65, no. 2, pp.
+   467-481, 2016.
+   (https://ieeexplore.ieee.org/document/7345567)
 
 """
 
 import numpy as np
-from ahrs.common.orientation import *
-from ahrs.common.mathfuncs import *
-from ahrs.common import DEG2RAD
+from ..common.orientation import q_prod, q2R
+
+GRAVITY = 9.80665
 
 class AQUA:
     """
@@ -31,41 +34,118 @@ class AQUA:
         Sampling rate in seconds. Inverse of sampling frequency.
 
     """
-    def __init__(self, *args, **kwargs):
-        self.input = args[0] if args else None
-        self.frequency = kwargs.get('frequency', 100.0)
-        self.Dt = kwargs.get('Dt', 1.0/self.frequency)
-        self.alpha = kwargs.get('alpha', 0.1)
-        self.threshold = kwargs.get('threshold', 0.1)
-        self.mdip = kwargs.get('magnetic_dip', 64.22)    # Magnetic dip, in degrees, in Munich, Germany.
-        self.q_I = np.array([1.0, 0.0, 0.0, 0.0])
-        self.m_ref = np.array([cosd(self.mdip), 0.0, -sind(self.mdip)])
-        # Process of data is given
-        if self.input:
-            self.Q = self.estimate_all()
+    def __init__(self, gyr: np.ndarray = None, acc: np.ndarray = None, mag: np.ndarray = None, **kw):
+        self.gyr = gyr
+        self.acc = acc
+        self.mag = mag
+        self.frequency = kw.get('frequency', 100.0)
+        self.Dt = kw.get('Dt', 1.0/self.frequency)
+        self.q0 = kw.get('q0')
+        self.alpha = kw.get('alpha', 0.01)
+        self.beta = kw.get('beta', 0.01)
+        self.threshold = kw.get('threshold', 0.9)
+        if self.acc is not None and self.gyr is not None:
+            self.Q = self._compute_all()
 
-    def estimate_all(self):
-        """
-        Estimate the quaternions given all data in class Data.
+    def _compute_all(self):
+        """Estimate all quaternions with given sensor values"""
+        if self.acc.shape != self.gyr.shape:
+            raise ValueError("acc and gyr are not the same size")
+        num_samples = len(self.gyr)
+        Q = np.zeros((num_samples, 4))
+        # Compute with IMU architecture
+        if self.mag is None:
+            Q[0] = self.init_q(self.acc[0]) if self.q0 is None else self.q0.copy()
+            for t in range(1, num_samples):
+                Q[t] = self.updateIMU(Q[t-1], self.gyr[t], self.acc[t])
+            return Q
+        # Compute with MARG architecture
+        if self.mag.shape != self.gyr.shape:
+            raise ValueError("mag and gyr are not the same size")
+        Q[0] = self.init_q(self.acc[0], self.mag[0]) if self.q0 is None else self.q0.copy()
+        for t in range(1, num_samples):
+            Q[t] = self.updateMARG(Q[t-1], self.gyr[t], self.acc[t], self.mag[t])
+        return Q
 
-        Class Data must have, at least, `gyr`, `acc` and `mag` attributes.
+    def init_q(self, acc, mag: np.ndarray = None):
+        ax, ay, az = acc.copy()/np.linalg.norm(acc)
+        # Quaternion from Accelerometer Readings (eq. 25)
+        if az>=0:
+            q_acc = np.array([np.sqrt((az+1)/2), -ay/np.sqrt(2*(1-ax)), ax/np.sqrt(2*(az+1)), 0.0])
+        else:
+            q_acc = np.array([-ay/np.sqrt(2*(1-az)), np.sqrt((1-az)/2.0), 0.0, ax/np.sqrt(2*(1-az))])
+        if mag is not None:
+            lx, ly, lz = q2R(q_acc).T@mag
+            Gamma = lx**2 + ly**2
+            # Quaternion from Magnetometer Readings (eq. 35)
+            if lx>=0:
+                q_mag = np.array([np.sqrt(Gamma+lx*np.sqrt(Gamma))/np.sqrt(2*Gamma), 0.0, 0.0, ly/np.sqrt(2)*np.sqrt(Gamma+lx*np.sqrt(Gamma))])
+            else:
+                q_mag = np.array([ly/np.sqrt(2)*np.sqrt(Gamma-lx*np.sqrt(Gamma)), 0.0, 0.0, np.sqrt(Gamma-lx*np.sqrt(Gamma))/np.sqrt(2*Gamma)])
+            # Generalized Quaternion Orientation (eq. 36)
+            q = q_prod(q_acc, q_mag)
+            return q/np.linalg.norm(q)
+        return q_acc
+
+    def _slerp(self, q, ratio, t):
+        q_I = np.array([1.0, 0.0, 0.0, 0.0])
+        if q[0]>t:
+            # LERP
+            q = (1.0-ratio)*q_I + ratio*q   # (eq. 50)
+        else:
+            # SLERP
+            angle = np.arccos(q[0])
+            q = q_I*np.sin(abs(1.0-ratio)*angle)/np.sin(angle) + q*np.sin(ratio*angle)/np.sin(angle)    # (eq. 52)
+        q /= np.linalg.norm(q)              # (eq. 51)
+        return q
+
+    def _adaptive_gain(self, gain, norm):
+        error = abs(norm-GRAVITY)/GRAVITY
+        e1, e2 = 0.1, 0.2
+        factor = 0.0
+        if error<e1:
+            factor = 1.0
+        if error<e2:
+            factor = (error-e1)/(e1-e2) + 1.0
+        return factor*gain
+
+    def updateIMU(self, q, gyr, acc):
+        """Update Quaternion
+
+        Parameters
+        ----------
+        q : array
+            A-priori quaternion.
+        gyr : array
+            Sample of tri-axial Gyroscope in radians.
+        acc : array
+            Sample of tri-axial Accelerometer.
 
         Returns
         -------
-        Q : array
-            M-by-4 Array with all estimated quaternions, where M is the number
-            of samples.
+        q : array
+            Estimated quaternion.
 
         """
-        data = self.input
-        Q = np.tile([1., 0., 0., 0.], (data.num_samples, 1))
-        if data.in_rads:
-            data.gyr *= DEG2RAD
-        for t in range(1, data.num_samples):
-            Q[t] = self.update(Q[t-1], data.gyr[t], data.acc[t], data.mag[t])
-        return Q
+        if not np.linalg.norm(gyr)>0:
+            return q
+        # PREDICTION
+        qDot = -0.5*q_prod([0, *gyr], q)                    # Quaternion derivative (eq. 38)
+        qInt = q + qDot*self.Dt                             # Quaternion integration (eq. 42)
+        qInt /= np.linalg.norm(qInt)
+        # CORRECTION
+        a_norm = np.linalg.norm(acc)
+        if not a_norm>0:
+            return qInt
+        a = acc.copy()/a_norm
+        gx, gy, gz = q2R(qInt).T@a                          # Predicted gravity (eq. 44)
+        q_acc = np.array([np.sqrt((gz+1)/2.0), -gy/np.sqrt(2.0*(gz+1)), gx/np.sqrt(2.0*(gz+1)), 0.0])     # Delta Quaternion (eq. 47)
+        # self.alpha = self._adaptive_gain(self.alpha, a_norm)
+        q_acc = self._slerp(q_acc, self.alpha, self.threshold)
+        q_prime = q_prod(qInt, q_acc)                       # (eq. 53)
+        return q_prime/np.linalg.norm(q_prime)
 
-    def update(self, q, gyr, acc, mag):
+    def updateMARG(self, q, gyr, acc, mag):
         """
         Update Quaternion
 
@@ -75,9 +155,9 @@ class AQUA:
             A-priori quaternion.
         gyr : array
             Sample of tri-axial Gyroscope in radians.
-        a : array
+        acc : array
             Sample of tri-axial Accelerometer.
-        m : array
+        mag : array
             Sample of tri-axial Magnetometer.
 
         Returns
@@ -86,46 +166,31 @@ class AQUA:
             Estimated quaternion.
 
         """
-        a = acc.copy()
-        m = mag.copy()
-        # Normalise acceleration and magnetic field measurements
-        a_norm = np.linalg.norm(a)
-        if a_norm == 0:
+        if not np.linalg.norm(gyr)>0:
             return q
-        a /= a_norm
-        m_norm = np.linalg.norm(m)
-        if m_norm == 0:
-            return q
-        m /= m_norm
-        # Predict
-        q_omega = q + 0.5*q_prod(q, np.concatenate(([0], gyr)))*self.Dt
-        q_omega /= np.linalg.norm(q_omega)
-        # # Acceleration Quaternion
-        gx, gy, gz = q2R(q_omega).T@a
-        Dq_acc = np.array([np.sqrt((gz+1.0)/2.0), -gy/np.sqrt(2.0*(gz+1.0)), gx/np.sqrt(2.0*(gz+1.0)), 0.0])
-        Dq_acc /= np.linalg.norm(Dq_acc)
-        # Correct
-        if Dq_acc[0] > self.threshold:
-            # Use LERP
-            Dq_acc = (1.0-self.alpha)*self.q_I + self.alpha*Dq_acc
-            Dq_acc /= np.linalg.norm(Dq_acc)
-        else:
-            # Use SLERP
-            Omega = np.arccos(np.dot(self.q_I, Dq_acc))
-            Dq_acc = self.q_I*np.sin(abs(1.0-self.alpha)*Omega)/np.sin(Omega) + Dq_acc*np.sin(self.alpha*Omega)/np.sin(Omega)
-        q = q_prod(q_omega, Dq_acc)
-        q /= np.linalg.norm(q)
-        # if az >= 0:
-        #     q_acc = np.array([np.sqrt((az+1.0)/2.0), -ay/np.sqrt(2.0*(az+1.0)), ax/np.sqrt(2.0*(az+1.0)), 0.0])
-        # else:
-        #     q_acc = np.array([-ay/np.sqrt(2.0*(1.0-az)), np.sqrt((1.0-az)/2.0), 0.0, ax/np.sqrt(2.0*(1.0-az))])
-        # # Magnetometer Quaternion
-        # mx, my, mz = m
-        # lx, ly, lz = q2R(q_acc).T@m
-        # G = lx**2 + ly**2
-        # if lx >= 0:
-        #     q_mag = np.array([np.sqrt(G+lx*np.sqrt(G))/np.sqrt(2.0*G), 0.0, 0.0, ly/(np.sqrt(2.0)*np.sqrt(G+lx*np.sqrt(G)))])
-        # else:
-        #     q_mag = np.array([ly/(np.sqrt(2.0)*np.sqrt(G-lx*np.sqrt(G))), 0.0, 0.0, np.sqrt(G-lx*np.sqrt(G))/np.sqrt(2.0*G)])
-        return q
-
+        # PREDICTION
+        qDot = -0.5*q_prod([0, *gyr], q)                    # Quaternion derivative (eq. 38)
+        qInt = q + qDot*self.Dt                             # Quaternion integration (eq. 42)
+        qInt /= np.linalg.norm(qInt)
+        # CORRECTION
+        # Accelerometer-Based
+        a_norm = np.linalg.norm(acc)
+        if not a_norm>0:
+            return qInt
+        a = acc.copy()/a_norm
+        gx, gy, gz = q2R(qInt).T@a                          # Predicted gravity (eq. 44)
+        q_acc = np.array([np.sqrt((gz+1)/2.0), -gy/np.sqrt(2.0*(gz+1)), gx/np.sqrt(2.0*(gz+1)), 0.0])     # Delta Quaternion (eq. 47)
+        # self.alpha = self._adaptive_gain(self.alpha, a_norm)
+        q_acc = self._slerp(q_acc, self.alpha, self.threshold)
+        q_prime = q_prod(qInt, q_acc)                       # (eq. 53)
+        q_prime /= np.linalg.norm(q_prime)
+        # Magnetometer-Based
+        m_norm = np.linalg.norm(mag)
+        if not m_norm>0:
+            return q_prime
+        lx, ly, lz = q2R(q_prime).T@mag                     # Predicted gravity (eq. 54)
+        Gamma = lx**2 + ly**2
+        q_mag = np.array([np.sqrt(Gamma+lx*np.sqrt(Gamma))/np.sqrt(2*Gamma), 0.0, 0.0, ly/np.sqrt(2*(Gamma+lx*np.sqrt(Gamma)))])    # (eq. 58)
+        q_mag = self._slerp(q_mag, self.beta, self.threshold)
+        q = q_prod(q_prime, q_mag)                          # (eq. 59)
+        return q/np.linalg.norm(q)