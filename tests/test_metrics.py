15cb341fc2b3215097a6a20b2af7e582a2334d8e
Mayitzin <mayitzin@gmail.com>
2021-10-19 00:34:22 +0200

    Start development of Unit tests.
    - Remove old unused test files.
    - Add unit test of function `euclidean` of submodule `metrics`.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Test Metrics
-
-"""
-
+#!/usr/bin/env python3
+import unittest
 import numpy as np
 import ahrs
 
-def test_dist(**kwargs):
-    """
-    Test Distance
-    """
-    a = np.random.random((2, 3))
-    d = ahrs.utils.metrics.euclidean(a[0], a[1])
-    result = np.allclose(d, np.linalg.norm(a[0] - a[1]))
-    return result
+class TestEuclidean(unittest.TestCase):
+    def test_correct_values(self):
+        self.assertEqual(ahrs.utils.euclidean(np.zeros(3), np.zeros(3)), 0.0)
+        self.assertAlmostEqual(ahrs.utils.euclidean(np.zeros(3), np.ones(3)), np.sqrt(3))
+        self.assertAlmostEqual(ahrs.utils.euclidean(np.ones(3), -np.ones(3)), 2.0*np.sqrt(3))
+        self.assertEqual(ahrs.utils.euclidean(np.array([1, 2, 3]), np.array([4, 5, 6])), 5.196152422706632)
+        self.assertGreaterEqual(ahrs.utils.euclidean(np.random.random(3)-0.5, np.random.random(3)-0.5), 0.0)
+
+    def test_invalid_values(self):
+        self.assertRaises(ValueError, ahrs.utils.euclidean, np.zeros(3), np.zeros(2))
+
+if __name__ == "__main__":
+    unittest.main()
