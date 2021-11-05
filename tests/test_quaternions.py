50f523f264f47603f09840dc4970df1db2b082c5
Mayitzin <mayitzin@gmail.com>
2021-11-05 22:04:40 +0100

    Add tests for inputs
    - Add testing wrong inputs.
    - Change assertEqual to assertAlmostEqual.
    - Add a decimal precision.

--- tests/test_quaternions.py
+++ tests/test_quaternions.py
@@ -12,6 +12,7 @@ class TestQuaternion(unittest.TestCase):
         self.vector4 = np.random.random(4)-0.5
         self.vector4 /= np.linalg.norm(self.vector4)
         self.q4 = ahrs.Quaternion(self.vector4)
+        self.decimal_precision = 15
 
     def test_identity_quaternion(self):
         self.assertEqual(self.q0.w, 1.0)
@@ -21,18 +22,35 @@ class TestQuaternion(unittest.TestCase):
         np.testing.assert_equal(self.q0.v, np.zeros(3))
 
     def test_versor_from_3d_array(self):
-        self.assertEqual(self.q3.w, 0.0)
-        self.assertEqual(self.q3.x, self.vector3[0])
-        self.assertEqual(self.q3.y, self.vector3[1])
-        self.assertEqual(self.q3.z, self.vector3[2])
-        np.testing.assert_equal(self.q3.v, self.vector3)
+        self.assertAlmostEqual(self.q3.w, 0.0, places=self.decimal_precision)
+        self.assertAlmostEqual(self.q3.x, self.vector3[0], places=self.decimal_precision)
+        self.assertAlmostEqual(self.q3.y, self.vector3[1], places=self.decimal_precision)
+        self.assertAlmostEqual(self.q3.z, self.vector3[2], places=self.decimal_precision)
+        np.testing.assert_almost_equal(self.q3.v, self.vector3, decimal=self.decimal_precision)
 
     def test_versor_from_4d_array(self):
-        self.assertEqual(self.q4.w, self.vector4[0])
-        self.assertEqual(self.q4.x, self.vector4[1])
-        self.assertEqual(self.q4.y, self.vector4[2])
-        self.assertEqual(self.q4.z, self.vector4[3])
-        np.testing.assert_equal(self.q4.v, self.vector4[1:])
+        self.assertAlmostEqual(self.q4.w, self.vector4[0], places=self.decimal_precision)
+        self.assertAlmostEqual(self.q4.x, self.vector4[1], places=self.decimal_precision)
+        self.assertAlmostEqual(self.q4.y, self.vector4[2], places=self.decimal_precision)
+        self.assertAlmostEqual(self.q4.z, self.vector4[3], places=self.decimal_precision)
+        np.testing.assert_almost_equal(self.q4.v, self.vector4[1:], decimal=self.decimal_precision)
+
+    def test_wrong_input_array(self):
+        self.assertRaises(TypeError, ahrs.Quaternion, True)
+        self.assertRaises(TypeError, ahrs.Quaternion, 3.0)
+        self.assertRaises(TypeError, ahrs.Quaternion, "[1.0, 2.0, 3.0, 4.0]")
+        self.assertRaises(ValueError, ahrs.Quaternion, np.random.random((6, 3)))
+        self.assertRaises(ValueError, ahrs.Quaternion, np.random.random((6, 4)))
+        self.assertRaises(ValueError, ahrs.Quaternion, np.random.random(2))
+        self.assertRaises(ValueError, ahrs.Quaternion, np.random.random(2).tolist())
+        self.assertRaises(ValueError, ahrs.Quaternion, np.zeros(3))
+
+    def test_wrong_input_dcm(self):
+        self.assertRaises(TypeError, ahrs.Quaternion, dcm=3)
+        self.assertRaises(TypeError, ahrs.Quaternion, dcm=3.0)
+        self.assertRaises(TypeError, ahrs.Quaternion, dcm="np.identity(3)")
+        self.assertRaises(ValueError, ahrs.Quaternion, dcm=np.random.random((3, 3)))
+        self.assertRaises(ValueError, ahrs.Quaternion, dcm=-np.identity(3))
 
 if __name__ == "__main__":
     unittest.main()
