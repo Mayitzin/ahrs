7b376c675e3aac309458e269d208b11bbaca038b
Mayitzin <mayitzin@gmail.com>
2021-11-17 23:44:08 +0100

    Add Unit Test for QUEST.

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -5,6 +5,7 @@ import ahrs
 
 wmm = ahrs.utils.WMM(latitude=ahrs.MUNICH_LATITUDE, longitude=ahrs.MUNICH_LONGITUDE, height=ahrs.MUNICH_HEIGHT)
 REFERENCE_MAGNETIC_VECTOR = np.array([wmm.X, wmm.Y, wmm.Z])
+GRAVITY = ahrs.utils.WGS().normal_gravity(ahrs.MUNICH_LATITUDE, ahrs.MUNICH_HEIGHT)
 
 class TestTRIAD(unittest.TestCase):
     def setUp(self) -> None:
@@ -64,6 +65,22 @@ class TestSAAM(unittest.TestCase):
         self.assertRaises(TypeError, ahrs.filters.SAAM, acc="self.Rg", mag="self.Rm")
         self.assertRaises(ValueError, ahrs.filters.SAAM, acc=[1.0, 2.0], mag=[2.0, 3.0, 4.0])
         self.assertRaises(ValueError, ahrs.filters.SAAM, acc=[1.0, 2.0, 3.0, 4.0], mag=[2.0, 3.0, 4.0, 5.0])
+
+class TestQUEST(unittest.TestCase):
+    def setUp(self) -> None:
+        # Create random attitudes
+        num_samples = 1000
+        self.Qts = ahrs.QuaternionArray(np.random.random((num_samples, 4)) - 0.5)
+        self.rotations = self.Qts.to_DCM()
+        # Add noise to reference vectors and rotate them by the random attitudes
+        noises = np.random.randn(2*num_samples, 3)*1e-3
+        self.Rg = np.array([R @ (np.array([0.0, 0.0, -GRAVITY]) + noises[i]) for i, R in enumerate(self.rotations)])
+        self.Rm = np.array([R @ (REFERENCE_MAGNETIC_VECTOR + noises[i+num_samples]) for i, R in enumerate(self.rotations)])
+        self.decimal_precision = 7e-2
+
+    def test_single_values(self):
+        quest = ahrs.filters.QUEST(self.Rg[0], self.Rm[0])
+        self.assertLess(ahrs.utils.metrics.qad(self.Qts[0], quest.Q), self.decimal_precision)
 
 if __name__ == '__main__':
     unittest.main()
