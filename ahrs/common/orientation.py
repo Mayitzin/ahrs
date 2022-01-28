b97d6d7f090a27fff76bfbeae05087760b6c9a5e
Mayitzin <mayitzin@gmail.com>
2022-01-28 16:36:55 +0100

    Remove unnecessary else and add handling of empty quaternion.

--- ahrs/common/orientation.py
+++ ahrs/common/orientation.py
@@ -1114,16 +1114,17 @@ def chiaverini(dcm: np.ndarray) -> np.ndarray:
         q[1] = 0.5*np.sign(dcm[2, 1]-dcm[1, 2])*np.sqrt(np.clip(dcm[0, 0]-dcm[1, 1]-dcm[2, 2], -1.0, 1.0)+1.0)
         q[2] = 0.5*np.sign(dcm[0, 2]-dcm[2, 0])*np.sqrt(np.clip(dcm[1, 1]-dcm[2, 2]-dcm[0, 0], -1.0, 1.0)+1.0)
         q[3] = 0.5*np.sign(dcm[1, 0]-dcm[0, 1])*np.sqrt(np.clip(dcm[2, 2]-dcm[0, 0]-dcm[1, 1], -1.0, 1.0)+1.0)
+        if not any(q):
+            q[0] = 1.0
         q /= np.linalg.norm(q)
         return q
-    else:
-        Q = np.zeros((dcm.shape[0], 4))
-        Q[:, 0] = 0.5*np.sqrt(dcm.trace(axis1=1, axis2=2) + 1.0)
-        Q[:, 1] = 0.5*np.sign(dcm[:, 2, 1] - dcm[:, 1, 2])*np.sqrt(np.clip(dcm[:, 0, 0]-dcm[:, 1, 1]-dcm[:, 2, 2], -1.0, 1.0) + 1.0)
-        Q[:, 2] = 0.5*np.sign(dcm[:, 0, 2] - dcm[:, 2, 0])*np.sqrt(np.clip(dcm[:, 1, 1]-dcm[:, 2, 2]-dcm[:, 0, 0], -1.0, 1.0) + 1.0)
-        Q[:, 3] = 0.5*np.sign(dcm[:, 1, 0] - dcm[:, 0, 1])*np.sqrt(np.clip(dcm[:, 2, 2]-dcm[:, 0, 0]-dcm[:, 1, 1], -1.0, 1.0) + 1.0)
-        Q /= np.linalg.norm(Q, axis=1)[:, None]
-        return Q
+    Q = np.zeros((dcm.shape[0], 4))
+    Q[:, 0] = 0.5*np.sqrt(dcm.trace(axis1=1, axis2=2) + 1.0)
+    Q[:, 1] = 0.5*np.sign(dcm[:, 2, 1] - dcm[:, 1, 2])*np.sqrt(np.clip(dcm[:, 0, 0]-dcm[:, 1, 1]-dcm[:, 2, 2], -1.0, 1.0) + 1.0)
+    Q[:, 2] = 0.5*np.sign(dcm[:, 0, 2] - dcm[:, 2, 0])*np.sqrt(np.clip(dcm[:, 1, 1]-dcm[:, 2, 2]-dcm[:, 0, 0], -1.0, 1.0) + 1.0)
+    Q[:, 3] = 0.5*np.sign(dcm[:, 1, 0] - dcm[:, 0, 1])*np.sqrt(np.clip(dcm[:, 2, 2]-dcm[:, 0, 0]-dcm[:, 1, 1], -1.0, 1.0) + 1.0)
+    Q /= np.linalg.norm(Q, axis=1)[:, None]
+    return Q
 
 def hughes(dcm: np.ndarray) -> np.ndarray:
     """
