9b87976e6376f7c408bc212044f4e7b4954cf839
Mayitzin <mayitzin@gmail.com>
2022-04-25 22:43:43 +0200

    Add unit tests for Fourati estimator

--- tests/test_estimators.py
+++ tests/test_estimators.py
@@ -628,6 +628,66 @@ class TestFourati(unittest.TestCase):
         orientation = ahrs.filters.Fourati(gyr=self.gyr, acc=self.Rg, mag=self.Rm)
         self.assertLess(np.nanmean(ahrs.utils.metrics.qad(self.Qts, orientation.Q)), self.noise_sigma*20)
 
+    def test_wrong_input_vectors(self):
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=1.0, acc=self.Rg, mag=self.Rm)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr="self.gyr", acc=self.Rg, mag=self.Rm)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=True, acc=self.Rg, mag=self.Rm)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=1.0, mag=2.0)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=2.0)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=1.0, mag=self.Rm)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc="self.Rg", mag="self.Rm")
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr[0], acc=self.Rg[0], mag=True)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=True, mag=[1.0, 2.0, 3.0])
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr[:2], acc=self.Rg, mag=self.Rm)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=[1.0, 2.0], mag=[2.0, 3.0, 4.0])
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=[1.0, 2.0, 3.0, 4.0], mag=[2.0, 3.0, 4.0, 5.0])
+
+    def test_wrong_input_frequency(self):
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, frequency="100.0")
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, frequency=[100.0])
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, frequency=(100.0,))
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, frequency=True)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, frequency=0.0)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, frequency=-100.0)
+
+    def test_wrong_input_Dt(self):
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, Dt="0.01")
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, Dt=[0.01])
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, Dt=(0.01,))
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, Dt=True)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, Dt=0.0)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, Dt=-0.01)
+
+    def test_wrong_input_gain(self):
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, gain="0.1")
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, gain=[0.1])
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, gain=(0.1,))
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, gain=True)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, gain="0.1")
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, gain=[0.1])
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, gain=(0.1,))
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, gain=True)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, gain=0.0)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, gain=-0.1)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, gain=0.0)
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, gain=-0.1)
+
+    def test_wrong_magnetic_dip(self):
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, magnetic_dip='34.5')
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, magnetic_dip=False)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, magnetic_dip=['34.5'])
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, magnetic_dip=('34.5',))
+
+    def test_wrong_initial_quaternion(self):
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=1)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=1.0)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=True)
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0="[1.0, 0.0, 0.0, 0.0]")
+        self.assertRaises(TypeError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=['1.0', '0.0', '0.0', '0.0'])
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=[1.0])
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=[1.0, 0.0, 0.0])
+        self.assertRaises(ValueError, ahrs.filters.Fourati, gyr=self.gyr, acc=self.Rg, mag=self.Rm, q0=np.zeros(4))
+
 class TestEKF(unittest.TestCase):
     def setUp(self) -> None:
         # Create random attitudes
