963da502cc926716b8bc9dc1b2ac219d7480d274
Mayitzin <mayitzin@gmail.com>
2022-05-12 17:36:22 +0200

    rename _assert_iterable as _assert_valid_array_type and add information about it.

--- ahrs/utils/core.py
+++ ahrs/utils/core.py
@@ -8,12 +8,15 @@ This module is private. All functions and objects are available in the main
 
 import numpy as np
 
-def _assert_iterable(item, item_name: str = 'iterable'):
+def _assert_valid_array_type(item, item_name: str = 'iterable'):
+    """Assert it is an iterable"""
+    # NOTE: This could be changed to a more pythonic solution looking for the
+    # dunder method __iter__(), but that yields strings too.
     if not isinstance(item, (list, tuple, np.ndarray)):
         raise TypeError(f"{item_name} must be given as an array. Got {type(item)}")
 
 def _assert_numerical_iterable(item, item_name: str = 'iterable'):
-    _assert_iterable(item, item_name)
+    _assert_valid_array_type(item, item_name)
     for i in item:
         if not isinstance(i, (int, float)):
             raise TypeError(f"{item_name} must be given as an array of numeric values. Got {type(i)}")
