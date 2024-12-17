d424e597f569f1bac3fce89e5e0b009e4c4d9857
Mayitzin <mayitzin@gmail.com>
2024-12-17 22:28:47 +0100

    Update tests for igf with newest values.

--- tests/test_wgs84.py
+++ tests/test_wgs84.py
@@ -9,8 +9,9 @@ class TestWELMEC(unittest.TestCase):
 
 class TestInternationalGravity(unittest.TestCase):
     def test_correct_values(self):
-        self.assertEqual(ahrs.utils.international_gravity(10.0), 9.781884110728155)
+        self.assertEqual(ahrs.utils.international_gravity(10.0), 9.781924844039924)
         self.assertEqual(ahrs.utils.international_gravity(10.0, epoch='1930'), 9.7820428934191)
+        self.assertEqual(ahrs.utils.international_gravity(10.0, epoch='1984'), 9.781882458192761)
 
 class TestWGS84(unittest.TestCase):
     def setUp(self) -> None:
