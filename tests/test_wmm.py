555b6afc4d0ffe5ae4b199171e50cfde0ad9694f
Mayitzin <mayitzin@gmail.com>
2025-01-29 20:59:33 +0100

    Add tests for WMM 2025.

--- tests/test_wmm.py
+++ tests/test_wmm.py
@@ -51,20 +51,22 @@ class TestWMM(unittest.TestCase):
         data : dict
             Dictionary with the test values.
         """
+        keys = ["date", "height", "latitude", "longitude", "X", "Y", "Z", "H",
+                "F", "I", "D", "GV", "dX", "dY", "dZ", "dH", "dF", "dI", "dD"]
+        if not filename.endswith(('.csv', '.txt')):
+            raise TypeError("File type is not supported. Try a csv or txt File.")
         if filename.endswith('.csv'):
             data = np.genfromtxt(filename, delimiter=';', skip_header=1)
             if data.shape[1] < 19:
                 raise ValueError(f"File '{filename}' has incomplete data.")
-            keys = ["date", "height", "latitude", "longitude", "X", "Y", "Z", "H", "F", "I", "D", "GV",
-                "dX", "dY", "dZ", "dH", "dF", "dI", "dD"]
         elif filename.endswith('.txt'):
             data = np.genfromtxt(filename, skip_header=1, comments='#')
             if data.shape[1] < 18:
                 raise ValueError(f"File '{filename}' has incomplete data.")
-            keys = ["date", "height", "latitude", "longitude", "D", "I", "H", "X", "Y", "Z", "F",
-                "dD", "dI", "dH", "dX", "dY", "dZ", "dF"]
-        else:
-            raise TypeError("File type is not supported. Try a csv or txt File.")
+            if '2020' in filename:
+                keys = ["date", "height", "latitude", "longitude", "D", "I",
+                        "H", "X", "Y", "Z", "F", "dD", "dI", "dH", "dX", "dY",
+                        "dZ", "dF"]
         return dict(zip(keys, data.T))
 
     def setUp(self):
@@ -88,6 +90,17 @@ class TestWMM(unittest.TestCase):
 
     def test_wmm2020(self):
         test_values = self._load_test_values(os.path.join(self.base_path, "../ahrs/utils/WMM2020/WMM2020_TEST_VALUES.txt"))
+        num_tests = len(test_values['date'])
+        for i in range(num_tests):
+            self.wmm.magnetic_field(test_values['latitude'][i], test_values['longitude'][i], test_values['height'][i], date=test_values['date'][i])
+            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['X'][i], self.wmm.X))
+            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Y'][i], self.wmm.Y))
+            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Z'][i], self.wmm.Z))
+            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['I'][i], self.wmm.I))
+            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['D'][i], self.wmm.D))
+
+    def test_wmm2025(self):
+        test_values = self._load_test_values(os.path.join(self.base_path, "../ahrs/utils/WMM2025/WMM2025_TEST_VALUES.txt"))
         num_tests = len(test_values['date'])
         for i in range(num_tests):
             self.wmm.magnetic_field(test_values['latitude'][i], test_values['longitude'][i], test_values['height'][i], date=test_values['date'][i])
