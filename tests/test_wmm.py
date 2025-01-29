12a1979e95ec776c0df84108221929c4dec91bef
Mayitzin <mayitzin@gmail.com>
2025-01-29 21:15:48 +0100

    Refactor messages of assertAlmostEqual in tests of WMM to use f-strings.

--- tests/test_wmm.py
+++ tests/test_wmm.py
@@ -51,10 +51,10 @@ class TestWMM(unittest.TestCase):
         data : dict
             Dictionary with the test values.
         """
+        if not filename.endswith(('.csv', '.txt')):
+            raise TypeError("File type is not supported. Try a csv or txt file.")
         keys = ["date", "height", "latitude", "longitude", "X", "Y", "Z", "H",
                 "F", "I", "D", "GV", "dX", "dY", "dZ", "dH", "dF", "dI", "dD"]
-        if not filename.endswith(('.csv', '.txt')):
-            raise TypeError("File type is not supported. Try a csv or txt File.")
         if filename.endswith('.csv'):
             data = np.genfromtxt(filename, delimiter=';', skip_header=1)
             if data.shape[1] < 19:
@@ -81,34 +81,34 @@ class TestWMM(unittest.TestCase):
         num_tests = len(test_values['date'])
         for i in range(num_tests):
             self.wmm.magnetic_field(test_values['latitude'][i], test_values['longitude'][i], test_values['height'][i], date=test_values['date'][i])
-            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['X'][i], self.wmm.X))
-            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Y'][i], self.wmm.Y))
-            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Z'][i], self.wmm.Z))
-            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['I'][i], self.wmm.I))
-            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['D'][i], self.wmm.D))
-            self.assertAlmostEqual(test_values['GV'][i], self.wmm.GV, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['GV'][i], self.wmm.GV))
+            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, f"Expected {test_values['X'][i]:.1f}, result {self.wmm.X:.1f}")
+            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, f"Expected {test_values['Y'][i]:.1f}, result {self.wmm.Y:.1f}")
+            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, f"Expected {test_values['Z'][i]:.1f}, result {self.wmm.Z:.1f}")
+            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, f"Expected {test_values['I'][i]:.2f}, result {self.wmm.I:.2f}")
+            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, f"Expected {test_values['D'][i]:.2f}, result {self.wmm.D:.2f}")
+            self.assertAlmostEqual(test_values['GV'][i], self.wmm.GV, 2, f"Expected {test_values['GV'][i]:.2f}, result {self.wmm.GV:.2f}")
 
     def test_wmm2020(self):
         test_values = self._load_test_values(os.path.join(self.base_path, "../ahrs/utils/WMM2020/WMM2020_TEST_VALUES.txt"))
         num_tests = len(test_values['date'])
         for i in range(num_tests):
             self.wmm.magnetic_field(test_values['latitude'][i], test_values['longitude'][i], test_values['height'][i], date=test_values['date'][i])
-            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['X'][i], self.wmm.X))
-            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Y'][i], self.wmm.Y))
-            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Z'][i], self.wmm.Z))
-            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['I'][i], self.wmm.I))
-            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['D'][i], self.wmm.D))
+            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, f"Expected {test_values['X'][i]:.1f}, result {self.wmm.X:.1f}")
+            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, f"Expected {test_values['Y'][i]:.1f}, result {self.wmm.Y:.1f}")
+            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, f"Expected {test_values['Z'][i]:.1f}, result {self.wmm.Z:.1f}")
+            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, f"Expected {test_values['I'][i]:.2f}, result {self.wmm.I:.2f}")
+            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, f"Expected {test_values['D'][i]:.2f}, result {self.wmm.D:.2f}")
 
     def test_wmm2025(self):
         test_values = self._load_test_values(os.path.join(self.base_path, "../ahrs/utils/WMM2025/WMM2025_TEST_VALUES.txt"))
         num_tests = len(test_values['date'])
         for i in range(num_tests):
             self.wmm.magnetic_field(test_values['latitude'][i], test_values['longitude'][i], test_values['height'][i], date=test_values['date'][i])
-            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['X'][i], self.wmm.X))
-            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Y'][i], self.wmm.Y))
-            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, 'Expected {:.1f}, result {:.1f}'.format(test_values['Z'][i], self.wmm.Z))
-            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['I'][i], self.wmm.I))
-            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, 'Expected {:.2f}, result {:.2f}'.format(test_values['D'][i], self.wmm.D))
+            self.assertAlmostEqual(test_values['X'][i], self.wmm.X, 1, f"Expected {test_values['X'][i]:.1f}, result {self.wmm.X:.1f}")
+            self.assertAlmostEqual(test_values['Y'][i], self.wmm.Y, 1, f"Expected {test_values['Y'][i]:.1f}, result {self.wmm.Y:.1f}")
+            self.assertAlmostEqual(test_values['Z'][i], self.wmm.Z, 1, f"Expected {test_values['Z'][i]:.1f}, result {self.wmm.Z:.1f}")
+            self.assertAlmostEqual(test_values['I'][i], self.wmm.I, 2, f"Expected {test_values['I'][i]:.2f}, result {self.wmm.I:.2f}")
+            self.assertAlmostEqual(test_values['D'][i], self.wmm.D, 2, f"Expected {test_values['D'][i]:.2f}, result {self.wmm.D:.2f}")
 
 if __name__ == '__main__':
     unittest.main()
